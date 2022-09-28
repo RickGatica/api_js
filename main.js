f2daaf4927cbb6d2b1986f8aae705455230e5ec4
RickGatica <psychofreud@outlook.cl>
2022-09-28 05:02:29 -0300

    desafio completado

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-
-let content = document.getElementById('content');
+let content = document.getElementById("content");
 let fragmento = document.createDocumentFragment();
-let url = 'https://larnu-dev-upy5mhs63a-rj.a.run.app/api/v1/categories';
+let url = "https://larnu-dev-upy5mhs63a-rj.a.run.app/api/v1/categories";
 
-function axisoLarnU(){
-  axios.get(url).then((response)=>{
-    let json = response.data.communityCategories;
-    json.forEach((e) => {
-      content.innerHTML += `
+function apiLarnu() {
+  axios
+    .get(url)
+    .then((response) => {
+      let json = response.data.communityCategories;
+      json.forEach((e) => {
+        content.innerHTML += `
       <div class="contenedor">
       <div class="contenedor__icono"><img src="${e.icon}"></div>
           <h2 class="contenedor__name">${e.name}</h2>
@@ -15,14 +16,13 @@ function axisoLarnU(){
           <p class="contenedor__user">Users: ${e.users}</p>
           <a href="https://es.larnu.com/" class="contenedor__link">Go to LarnU</a>
       </div>
-      `
+      `;
+      });
     })
-  })
-  .catch((error) =>{
-    let message = error.statusText || "Ha ocurrido un error :( , lo sentimos";
-    content.innerHTML = `Error ${error.status}: ${message}`
-  })
+    .catch((error) => {
+      let message = error.statusText || "Ha ocurrido un error :( , lo sentimos";
+      content.innerHTML = `Error ${error.status}: ${message}`;
+    });
 }
 
-  axisoLarnU();
-
+apiLarnu();
